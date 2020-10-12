b80fe6254fddbfc6673631c5b127bc0881f062cd
Ben Gao <lambba@benxgao.com>
2020-10-13 12:17:07 +1300

    Refactor serverless config to fit Dynamodb and env

--- local-workflow/todos/create.js
+++ local-workflow/todos/create.js
@@ -1,5 +1,5 @@
-// const uuid = require('uuid');
-// const dynamodb = require('./dynamodb');
+const uuid = require('uuid');
+const dynamodb = require('./dynamodb');
 
 module.exports.create = (_event, _context, callback) => {
   const mockMessage = {
@@ -20,46 +20,4 @@ module.exports.create = (_event, _context, callback) => {
   console.log('response', response);
 
   callback(null, response);
-
-  // const timestamp = new Date().getTime();
-  // const data = JSON.parse(event.body);
-  // if (typeof data.text !== 'string') {
-  //   console.error('Validation Failed');
-  //   callback(null, {
-  //     statusCode: 400,
-  //     headers: { 'Content-Type': 'text/plain' },
-  //     body: "Couldn't create the todo item.",
-  //   });
-  //   return;
-  // }
-
-  // const params = {
-  //   TableName: process.env.DYNAMODB_TABLE,
-  //   Item: {
-  //     id: uuid.v1(),
-  //     text: data.text,
-  //     checked: false,
-  //     createdAt: timestamp,
-  //     updatedAt: timestamp,
-  //   },
-  // };
-
-  // dynamodb.put(params, (error) => {
-  // handle potential errors
-  // if (error) {
-  //   console.error(error);
-  //   callback(null, {
-  //     statusCode: error.statusCode || 501,
-  //     headers: { 'Content-Type': 'text/plain' },
-  //     body: "Couldn't create the todo item.",
-  //   });
-  //   return;
-  // }
-
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify(params.Item),
-  // };
-  // callback(null, response);
-  // });
 };
