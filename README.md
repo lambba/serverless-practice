f19829957c6692a5177a28f4c3163207dcf26ee4
Ben Gao <lambba@benxgao.com>
2020-10-17 21:04:21 +1300

    Update READEME and project desc

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# excutes-services
+# Serverless practice
+
+To get familiar with Serverless framework and AWS lambda & DynamoDB.
+
+The repository code was composed by a couple of open source project under MIT licenses:
+
+- [Serverless Architecture Boilerplate](https://github.com/msfidelis/serverless-architecture-boilerplate)
+
+- [Serverless official examples](https://github.com/serverless/examples/tree/9ef1b79e53b982c1de12345daf778ce3451426b2)
+
+- [Serverless community examples](https://github.com/serverless/examples/blob/9ef1b79e53b982c1de12345daf778ce3451426b2/community-examples.json#L1)
+
+- [Serverless Architecture Boilerplate](https://github.com/msfidelis/serverless-architecture-boilerplate)
--- serverless-dynamodb/todos/hello/index.js
+++ serverless-dynamodb/todos/hello/index.js
@@ -1,11 +1,5 @@
-import runWarm from '../../utils/runWarm';
-
-console.log('HELLO', process.env.HELLO);
-console.log('DYNAMO_TABLE_NOTES', process.env.DYNAMO_TABLE_NOTES);
-
 export const hello = async function hello(event, _context, _callback) {
   const response = {
-    statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
@@ -16,7 +10,3 @@ export const hello = async function hello(event, _context, _callback) {
 
   return response;
 };
-
-// Export the hander wrapped in the "run warm" utility which will handle events
-// from the scheduler, keeping our actual handler logic clean.
-// export default runWarm(hello);
